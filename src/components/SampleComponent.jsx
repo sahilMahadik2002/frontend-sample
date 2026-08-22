e4d89fe8bf709e0e52f38f427e0fcf437bdd0542
sahilMahadik2002 <sahil.m1@sankeysolutions.com>
2026-08-22 10:45:05 +0530

    Update SampleComponent.jsx

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -1,1 +1,15 @@
 
+// UserProfile.jsx
+
+import React from "react";
+
+function UserProfile({ user }) {
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
