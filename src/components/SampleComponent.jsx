6fe2298402668a2a0c089aed1cf6d7532210863a
sahilMahadik2002 <sahil.m1@sankeysolutions.com>
2026-09-05 11:07:10 +0530

    Duplicate 'Added One More PR' headings in UserProfile
    Added multiple identical headings to UserProfile component.

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -7,6 +7,9 @@ function UserProfile({ user }) {
   return (
     <div>
       <h1>Added One More PR</h1>
+      <h1>Added One More PR</h1>
+      <h1>Added One More PR</h1>
+      <h1>Added One More PR</h1>
       <h2>{user.name}</h2>
       <p>{user.email}</p>
     </div>
