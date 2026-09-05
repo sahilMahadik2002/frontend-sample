8537ae552d4e8408bff25d6e5951880f6d548488
sahilMahadik2002 <sahil.m1@sankeysolutions.com>
2026-09-05 11:05:50 +0530

    Add header to UserProfile component

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -6,6 +6,7 @@ import React from "react";
 function UserProfile({ user }) {
   return (
     <div>
+      <h1>Added One More PR</h1>
       <h2>{user.name}</h2>
       <p>{user.email}</p>
     </div>
